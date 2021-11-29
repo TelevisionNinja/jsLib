771ddb5040dfba26ea5d06cad0a40724a44e1abc
TelevisionNinja <TelevisionNinja@gmail.com>
2021-11-29 00:23:46 -0600

    added more functions

--- src/stringUtils.js
+++ src/stringUtils.js
@@ -1,3 +1,12 @@
+const whitespaceChars = new Set([
+    '\n',
+    '\r',
+    '\t',
+    ' ',
+    '\v',
+    '\f'
+]);
+
 /**
  * trims a specified character from a string
  * 
@@ -29,6 +38,49 @@ export function trimChar(str, trimChar) {
 }
 
 /**
+ * trims a specified character from the left side of a string
+ * 
+ * @param {*} str str to be trimmmed
+ * @param {*} trimChar char that will be trimed off of the str
+ * @returns 
+ */
+export function trimCharLeft(str, trimChar) {
+    let start = 0;
+    const len = str.length;
+
+    while (start < len) {
+        if (str[start] != trimChar) {
+            return str.substring(start);
+        }
+
+        ++start;
+    }
+
+    return '';
+}
+
+/**
+ * trims a specified character from the right side of a string
+ * 
+ * @param {*} str str to be trimmmed
+ * @param {*} trimChar char that will be trimed off of the str
+ * @returns 
+ */
+export function trimCharRight(str, trimChar) {
+    let end = str.length;
+
+    while (end) {
+        --end;
+
+        if (str[end] != trimChar) {
+            return str.substring(0, end + 1);
+        }
+    }
+
+    return '';
+}
+
+/**
  * trims a substring from a string
  * 
  * @param {*} str str to be trimmmed
@@ -54,8 +106,8 @@ export function trimSubstr(str, trimSubstr) {
         }
 
         if (!substrIndex) {
-            end = strIndex,
-                substrIndex = substrLen;
+            end = strIndex;
+            substrIndex = substrLen;
         }
     }
 
@@ -64,19 +116,88 @@ export function trimSubstr(str, trimSubstr) {
     }
 
     let start = 0;
-    strIndex = 0,
-        substrIndex = 0;
+    strIndex = 0;
+    substrIndex = 0;
     while (str[strIndex] == trimSubstr[substrIndex]) {
         ++strIndex;
         ++substrIndex;
 
         if (substrIndex == substrLen) {
-            start = strIndex,
-                substrIndex = 0;
+            start = strIndex;
+            substrIndex = 0;
         }
     }
 
     return str.substring(start, end);
+}
+
+/**
+ * trims a substring from the left side of a string
+ * 
+ * @param {*} str str to be trimmmed
+ * @param {*} trimSubstr substring that will be trimed off of the str
+ * @returns 
+ */
+export function trimSubstrLeft(str, trimSubstr) {
+    const substrLen = trimSubstr.length,
+        len = str.length;
+
+    if (!substrLen || len < substrLen) {
+        return str;
+    }
+
+    let start = 0,
+        strIndex = 0,
+        substrIndex = 0;
+    while (start < len) {
+        if (str[strIndex] != trimSubstr[substrIndex]) {
+            return str.substring(start);
+        }
+
+        ++strIndex;
+        ++substrIndex;
+
+        if (substrIndex == substrLen) {
+            start = strIndex;
+            substrIndex = 0;
+        }
+    }
+
+    return '';
+}
+
+/**
+ * trims a substring from the right side of a string
+ * 
+ * @param {*} str str to be trimmmed
+ * @param {*} trimSubstr substring that will be trimed off of the str
+ * @returns 
+ */
+export function trimSubstrRight(str, trimSubstr) {
+    const substrLen = trimSubstr.length;
+    let end = str.length;
+
+    if (!substrLen || end < substrLen) {
+        return str;
+    }
+
+    let strIndex = end,
+        substrIndex = substrLen;
+    while (strIndex) {
+        --strIndex;
+        --substrIndex;
+
+        if (str[strIndex] != trimSubstr[substrIndex]) {
+            return str.substring(0, end);
+        }
+
+        if (!substrIndex) {
+            end = strIndex;
+            substrIndex = substrLen;
+        }
+    }
+
+    return '';
 }
 
 /**
@@ -138,13 +259,13 @@ export function includesPhrase1(str, phrase, caseSensitive) {
     let isSpace = true;
 
     for (let i = 0; i <= limit; i++) {
-        if (str[i] == ' ') {
+        if (whitespaceChars.has(str[i])) {
             isSpace = true;
         }
         else if (isSpace) {
             isSpace = false;
 
-            if ((i < limit && str[i + substrLen] == ' ') || i == limit) {
+            if (i == limit || whitespaceChars.has(str[i + substrLen])) {
                 let j = 0;
 
                 while (j < substrLen && str[i + j] == phrase[j]) {
@@ -230,6 +351,81 @@ export function trimCharArr(str, charArr) {
 }
 
 /**
+ * trims a set of characters from a string
+ * 
+ * @param {*} str str to be trimmmed
+ * @param {*} charSet chars that will be trimed off of the str
+ * @returns 
+ */
+export function trimCharSet(str, charSet) {
+    let end = str.length;
+
+    while (end) {
+        --end;
+ 
+        if (!charSet.has(str[end])) {
+            ++end;
+            break;
+        }
+    }
+
+    if (!end) {
+        return '';
+    }
+
+    let start = 0;
+
+    while (charSet.has(str[start])) {
+        ++start;
+    }
+
+    return str.substring(start, end);
+}
+
+/**
+ * trims a set of characters from the left side of a string
+ * 
+ * @param {*} str str to be trimmmed
+ * @param {*} charSet chars that will be trimed off of the str
+ * @returns 
+ */
+export function trimCharSetLeft(str, charSet) {
+    let start = 0;
+    const len = str.length;
+
+    while (start < len) {
+        if (!charSet.has(str[start])) {
+            return str.substring(start);
+        }
+
+        ++start;
+    }
+
+    return '';
+}
+
+/**
+ * trims a set of characters from the right side of a string
+ * 
+ * @param {*} str str to be trimmmed
+ * @param {*} charSet chars that will be trimed off of the str
+ * @returns 
+ */
+export function trimCharSetRight(str, charSet) {
+    let end = str.length;
+
+    while (end) {
+        --end;
+
+        if (!charSet.has(str[end])) {
+            return str.substring(0, end + 1);
+        }
+    }
+
+    return '';
+}
+
+/**
  * trims a list of substrings from a string
  * 
  * @param {*} str 
@@ -239,53 +435,40 @@ export function trimCharArr(str, charArr) {
 export function trimSubstrArr(str, substrArr) {
     const substrsLen = substrArr.length;
 
-    /*
-    reject substring arrays with empty strings, (assumes the list is not sorted by length)
-    use this if statement instead of the for loop if the array will always be sorted:
-
-    if (!substrsLen || !substrArr[0].length) {
+    if (!substrsLen || !substrArr[substrsLen - 1].length) {
         return str;
     }
-    */
-    for (let i = 0; i < substrsLen; i++) {
-        if (!substrArr[i].length) {
-            return str;
-        }
-    }
 
-    let end = str.length;
+    let end = str.length,
+        i = 0;
 
-    while (end) {
-        let i = 0;
+    while (i < substrsLen) {
+        const substr = substrArr[i];
+        let substrIndex = substr.length;
 
-        while (i < substrsLen) {
-            const substr = substrArr[i];
-            let substrIndex = substr.length;
+        if (substrIndex <= end) {
+            let strIndex = end;
 
-            if (substrIndex <= end) {
-                let strIndex = end;
+            while (substrIndex) {
+                --strIndex;
+                --substrIndex;
 
-                while (substrIndex) {
-                    --strIndex;
-                    --substrIndex;
-
-                    if (str[strIndex] != substr[substrIndex]) {
-                        ++substrIndex;
-                        break;
-                    }
-                }
-
-                if (!substrIndex) {
-                    end = strIndex;
+                if (str[strIndex] != substr[substrIndex]) {
+                    ++substrIndex;
                     break;
                 }
             }
 
-            i++;
+            if (!substrIndex) {
+                end = strIndex;
+                i = 0;
+            }
+            else {
+                i++;
+            }
         }
-
-        if (i == substrsLen) {
-            break;
+        else {
+            i++;
         }
     }
 
@@ -295,39 +478,130 @@ export function trimSubstrArr(str, substrArr) {
 
     let start = 0,
         len = end;
-    while (true) {
-        let i = 0;
 
-        while (i < substrsLen) {
-            const substr = substrArr[i];
-            const substrLen = substr.length;
+    i = 0;
 
-            i++;
+    while (i < substrsLen) {
+        const substr = substrArr[i];
+        const substrLen = substr.length;
 
-            if (substrLen < len) {
-                let strIndex = start,
-                    substrIndex = 0;
+        i++;
 
-                while (str[strIndex] == substr[substrIndex]) {
-                    ++strIndex;
-                    ++substrIndex;
+        if (substrLen < len) {
+            let strIndex = start,
+                substrIndex = 0;
 
-                    if (substrIndex == substrLen) {
-                        start = strIndex;
-                        len = end - start;
-                        i = 0;
-                        break;
-                    }
+            while (str[strIndex] == substr[substrIndex]) {
+                ++strIndex;
+                ++substrIndex;
+
+                if (substrIndex == substrLen) {
+                    start = strIndex;
+                    len = end - start;
+                    i = 0;
+                    break;
                 }
             }
-        }
-
-        if (i == substrsLen) {
-            break;
         }
     }
 
     return str.substring(start, end);
+}
+
+/**
+ * trims a list of substrings from the left side of a string
+ * 
+ * @param {*} str 
+ * @param {*} substrArr substrings that will be trimed off of the str, must be sorted from longest to shortest
+ * @returns 
+ */
+export function trimSubstrArrLeft(str, substrArr) {
+    const substrsLen = substrArr.length;
+
+    if (!substrsLen || !substrArr[substrsLen - 1].length) {
+        return str;
+    }
+
+    const end = str.length;
+    let start = 0,
+        len = end,
+        i = 0;
+
+    while (i < substrsLen) {
+        const substr = substrArr[i];
+        const substrLen = substr.length;
+
+        i++;
+
+        if (substrLen <= len) {
+            let strIndex = start,
+                substrIndex = 0;
+
+            while (str[strIndex] == substr[substrIndex]) {
+                ++strIndex;
+                ++substrIndex;
+
+                if (substrIndex == substrLen) {
+                    start = strIndex;
+                    len = end - start;
+                    i = 0;
+                    break;
+                }
+            }
+        }
+    }
+
+    return str.substring(start);
+}
+
+/**
+ * trims a list of substrings from the right side of a string
+ * 
+ * @param {*} str 
+ * @param {*} substrArr substrings that will be trimed off of the str, must be sorted from longest to shortest
+ * @returns 
+ */
+export function trimSubstrArrRight(str, substrArr) {
+    const substrsLen = substrArr.length;
+
+    if (!substrsLen || !substrArr[substrsLen - 1].length) {
+        return str;
+    }
+
+    let end = str.length,
+        i = 0;
+
+    while (i < substrsLen) {
+        const  substr = substrArr[i];
+        let substrIndex = substr.length;
+
+        if (substrIndex <= end) {
+            let strIndex = end;
+
+            while (substrIndex) {
+                --strIndex;
+                --substrIndex;
+
+                if (str[strIndex] != substr[substrIndex]) {
+                    ++substrIndex;
+                    break;
+                }
+            }
+
+            if (!substrIndex) {
+                end = strIndex;
+                i = 0;
+            }
+            else {
+                i++;
+            }
+        }
+        else {
+            i++;
+        }
+    }
+
+    return str.substring(0, end);
 }
 
 /**
@@ -415,7 +689,7 @@ export function cmdLnToArgArr(cmdLn, throwError = true) {
  * @param {*} str 
  * @returns 
  */
-export function properCase(str) {
+export function toProperCase(str) {
     let proper = '';
     let isSpace = true;
     const n = str.length;
@@ -424,7 +698,7 @@ export function properCase(str) {
     while (i < n) {
         let c = str[i];
 
-        if (c === ' ') {
+        if (whitespaceChars.has(c)) {
             isSpace = true;
         }
         else if (isSpace) {
@@ -440,4 +714,128 @@ export function properCase(str) {
     }
 
     return proper;
+}
+
+/**
+ * 
+ * @param {*} str1
+ * @param {*} str2
+ * @returns
+ * 		combined str of 2 strings that are alternated w/ each other
+ */
+export function alternateStrings(str1, str2) {
+    const str1Len = str1.length,
+        str2Len = str2.length;
+    let alternate = "";
+    let x = 0,
+        length;
+
+    if (str1Len > str2Len) {
+        length = str2Len;
+    }
+    else {
+        length = str1Len;
+    }
+
+    //----------------------------
+
+    while (x < length) {
+        alternate += str1[x];
+        alternate += str2[x];
+        x++;
+    }
+
+    if (x < str1Len) {
+        alternate += str1.substring(x);
+    }
+    else {
+        alternate += str2.substring(x);
+    }
+
+    // while (x < str1Len) {
+    //     alternate += str1[x];
+    //     x++;
+    // }
+
+    // while (x < str2Len) {
+    //     alternate += str2[x];
+    //     x++;
+    // }
+
+    return alternate;
+}
+
+/**
+ * finds the first instance of the substring
+ * 
+ * this implementation jumps to the second occurance of the substring's first char when a comparison failed in the substring comparison loop
+ * 
+ * @param {*} str 
+ * @param {*} substr 
+ * @param {*} index starting index
+ * @param {*} includeOverlap 
+ * @returns index of the substring, returns an empty vector if not found
+ */
+export function indexOfAll(str, substr, index = 0, includeOverlap = true) {
+    let finds = [];
+
+    if (index < 0) {
+        return finds;
+    }
+
+    const substrLen = substr.length;
+
+    if (!substrLen) {
+        finds.push(0);
+        return finds;
+    }
+
+    const firstChar = substr[0];
+    let i = index;
+    const strLen = str.length - substrLen;
+
+    while (i <= strLen) {
+        if (str[i] == firstChar) {
+            let j = 1,
+                indexSkip = 0;
+
+            while (j < substrLen) {
+                const compareChar = str[i + j];
+
+                if (!indexSkip && compareChar == firstChar) {
+                    indexSkip = j;
+                }
+
+                if (compareChar == substr[j]) {
+                    j++;
+                }
+                else {
+                    if (!indexSkip) {
+                        i += j + 1;
+                    }
+                    else {
+                        i += indexSkip;
+                    }
+
+                    break;
+                }
+            }
+
+            if (j == substrLen) {
+                finds.push(i);
+
+                if (includeOverlap) {
+                    i++;
+                }
+                else {
+                    i += substrLen;
+                }
+            }
+        }
+        else {
+            i++;
+        }
+    }
+
+    return finds;
 }
