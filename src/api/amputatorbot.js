dea35fe2250b0c836eb3e1a6314c15a69e6562e0
TelevisionNinja <TelevisionNinja@gmail.com>
2022-08-07 19:27:18 -0500

    param is an array

--- src/api/amputatorbot.js
+++ src/api/amputatorbot.js
@@ -1,8 +1,5 @@
 import PQueue from 'p-queue';
-import {
-    backOffFetch,
-    extractAmpUrls
-} from '../urlUtils.js';
+import { backOffFetch } from '../urlUtils.js';
 
 const queue = new PQueue({
     interval: 1000,
@@ -11,32 +8,33 @@ const queue = new PQueue({
 
 /**
  * 
- * @param {*} str string containing urls
+ * @param {*} array url array
  * @param {*} guessAndCheck 
  * @param {*} maxDepth 
  * @returns 
  */
-export async function getCanonicals(str, guessAndCheck = true, maxDepth = 3) {
-    let linksQuery = [...extractAmpUrls(str).values()].join(';');
+export async function getCanonicals(array, guessAndCheck = true, maxDepth = 3) {
     let canonicals = [];
 
-    await queue.add(async () => {
-        const response = await fetch(`https://www.amputatorbot.com/api/v1/convert?gac=${guessAndCheck}&md=${maxDepth}&q=${linksQuery}`);
-
-        if (backOffFetch(response, queue)) {
-            return;
-        }
-
-        const linkArray = await response.json();
-
-        for (let i = 0; i < linkArray.length; i++) {
-            const linkObject = linkArray[i];
-
-            if (linkObject.canonical !== null) {
-                canonicals.push(linkObject.canonical.url);
+    if (array.length) {
+        await queue.add(async () => {
+            const response = await fetch(`https://www.amputatorbot.com/api/v1/convert?gac=${guessAndCheck}&md=${maxDepth}&q=${array.join(';')}`);
+    
+            if (backOffFetch(response, queue)) {
+                return;
             }
-        }
-    });
+    
+            const linkArray = await response.json();
+    
+            for (let i = 0; i < linkArray.length; i++) {
+                const linkObject = linkArray[i];
+    
+                if (linkObject.canonical !== null) {
+                    canonicals.push(linkObject.canonical.url);
+                }
+            }
+        });
+    }
 
     return canonicals;
 }
